fb1db4cf2bf3c6a5b337d2dfe7eb212af1ddb3bc
Nikhil Anil Prakash <95761623+NikhilAPrakash@users.noreply.github.com>
2025-11-30 09:29:49 -0500

    ..

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio_NAP/",  // <-- repo name here
+  base: "/",  // <-- repo name here
 })
